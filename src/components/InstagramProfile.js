c21fe8bb58f67b812ce2fb66f29c73b3bffadb62
cecile lesgourgues <cecile.lesgourgues@ekino.com>
2019-09-25 23:54:55 +0200

    fix instagram profile

--- src/components/InstagramProfile.js
+++ src/components/InstagramProfile.js
@@ -5,19 +5,25 @@ import Link from "gatsby-link";
 const InstagramProfile = ({ profile }) => (
   <div className="Instagram__profile">
     <div className="Instagram__profile__image">
-      <Img fixed={{ src: profile.profile_pic_url_hd, width: "180px", height: "180px" }} style={{border: "1px solid rgba(0,0,0,.0975)", borderRadius: "50%"}}/>
+      <Img
+        fixed={{ src: profile.profile_pic_url_hd, width: "120px", height: "120px" }}
+        style={{ border: "1px solid rgba(0,0,0,.0975)", borderRadius: "50%" }}
+      />
     </div>
     <div className="Instagram__profile__content">
-      <div className="Instagram__profile__name">
-        <span>
-          {profile.full_name} @{profile.username}
-        </span>
-        <button className="Instagram__profile__button">Follow</button>
+      <div className="Instagram__profile__id">
+        <span>{profile.full_name}</span>
+        <div className="Instagram__profile__followme">
+          <button className="Instagram__profile__button">Follow</button>
+          <span>@{profile.username}</span>
+        </div>
       </div>
       <span>{profile.biography}</span>
-      <div className="Instagram__profile__follow">
-        <span>{profile.edge_followed_by.count} followers</span>
-        <span>{profile.edge_follow.count} following</span>
+      <div>
+        <span className="Instagram__profile__follow">
+          {profile.edge_followed_by.count} followers
+        </span>
+        <span className="Instagram__profile__follow">{profile.edge_follow.count} following</span>
       </div>
     </div>
   </div>
