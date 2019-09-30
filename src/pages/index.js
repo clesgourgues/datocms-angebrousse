29e7f0e76be83c3c0ba6e06587fedb458e586fec
cecile lesgourgues <cecile.lesgourgues@ekino.com>
2019-09-30 17:18:34 +0200

    add eshop as homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
+
 import Layout from "../layouts/index";
+import Catalogue from "../components/Catalogue";
 
 export default () => (
   <StaticQuery
     query={graphql`
-      query {
-        site {
-          siteMetadata {
-            siteName
+      query CatalogueQuery {
+        products: allDatoCmsProduct {
+          edges {
+            node {
+              id
+              name
+              price
+              slug
+              category {
+                name
+              }
+              image {
+                url
+                sizes(maxWidth: 300, imgixParams: { fm: "jpg" }) {
+                  ...GatsbyDatoCmsSizes
+                }
+              }
+            }
+          }
+        }
+        filters: allDatoCmsCategory {
+          edges {
+            node {
+              name
+            }
           }
         }
       }
     `}
-    render={data => <div className="Intro"></div>}
+    render={data => <Catalogue products={data.products.edges} filters={data.filters.edges} />}
   />
 );
