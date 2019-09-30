d4790dcfb69c67694fabda5f28f0ba301689b950
cecile lesgourgues <cecile.lesgourgues@ekino.com>
2019-09-30 10:57:07 +0200

    add close button on encart

--- src/components/Encart.js
+++ src/components/Encart.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Encart = ({ encart }) => (
-  <div className="Encart">
-    <p>{encart.info}</p>
-  </div>
-);
+const Encart = ({ encart }) => {
+  const [closed, setClosed] = useState(false);
+  if (closed) {
+    return null;
+  }
+  return (
+    <div className="Encart">
+      <p>{encart.info}</p>
+      <span className="Encart__close" onClick={() => setClosed(true)}>
+        X
+      </span>
+    </div>
+  );
+};
 
 export default Encart;
