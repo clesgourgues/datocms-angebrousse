c97ddcfa407c87ea41199b0df6a5ed555438696d
cecile lesgourgues <cecile.lesgourgues@ekino.com>
2019-10-07 09:44:13 +0200

    refactor to hooks

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -1,38 +1,33 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import Filters from "../components/Filters";
 import CatalogueProduct from "../components/CatalogueProduct";
 
-class Catalogue extends Component {
-  state = {
-    selected: "tout voir"
+const Catalogue = ({ products, filters }) => {
+  let [selected, setSelected] = useState("tout voir");
+
+  const onMenuClick = selected => {
+    setSelected(selected);
   };
 
-  onMenuClick = selected => {
-    this.setState({ selected });
-  };
+  const productsToShow =
+    selected === "tout voir"
+      ? products
+      : products.filter(({ node: product }) => product.category[0].name === selected);
 
-  render() {
-    const { products, filters } = this.props;
-    const { selected } = this.state;
-    const productsToShow =
-      selected === "tout voir"
-        ? products
-        : products.filter(({ node: product }) => product.category[0].name === selected);
-    return (
-      <div className="Catalogue">
-        <div className="Wrap">
-          <h2 className="Title">E-shop</h2>
-          <Filters filters={filters} selected={selected} onClick={this.onMenuClick} />
-          <div className="Catalogue__products">
-            {productsToShow.map(({ node: product }) => (
-              <CatalogueProduct product={product} key={product.id} />
-            ))}
-          </div>
+  return (
+    <div className="Catalogue">
+      <div className="Wrap">
+        <h2 className="Title">E-shop</h2>
+        <Filters filters={filters} selected={selected} onClick={onMenuClick} />
+        <div className="Catalogue__products">
+          {productsToShow.map(({ node: product }) => (
+            <CatalogueProduct product={product} key={product.id} />
+          ))}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Catalogue;
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,60 +1,54 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-class ContactForm extends Component {
-  state = {
-    email: ""
-  };
+const ContactForm = ({ text }) => {
+  let [email, setEmail] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     document.addEventListener("snipcart.ready", () => {
       const user = window.Snipcart.api.user.current();
       if (user) {
-        this.setState({ email: user.email });
+        setEmail(user.email);
       }
     });
-  }
+  }, []);
 
-  handleChange = event => {
-    this.setState({ email: event.target.value });
+  const handleChange = event => {
+    setEmail(event.target.value);
   };
 
-  render() {
-    const { email } = this.state;
-    const { text } = this.props;
-    return (
-      <form
-        name="contact"
-        method="POST"
-        data-netlify-recaptcha="true"
-        data-netlify="true"
-        action="/success"
-        className="Contact__form"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <input
-          className="Contact__form__input"
-          name="email"
-          placeholder={text.emailPlaceholderText}
-          required
-          type="email"
-          maxLength="100"
-          value={email}
-          onChange={this.handleChange}
-        />
-        <textarea
-          name="message"
-          className="Contact__form__text"
-          placeholder={text.messagePlaceholderText}
-          required
-          maxLength="1000"
-        />
-        <div data-netlify-recaptcha="true"></div>
-        <button type="submit" className="Contact__form__button">
-          {text.sendButtonText}
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form
+      name="contact"
+      method="POST"
+      data-netlify-recaptcha="true"
+      data-netlify="true"
+      action="/success"
+      className="Contact__form"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <input
+        className="Contact__form__input"
+        name="email"
+        placeholder={text.emailPlaceholderText}
+        required
+        type="email"
+        maxLength="100"
+        value={email}
+        onChange={handleChange}
+      />
+      <textarea
+        name="message"
+        className="Contact__form__text"
+        placeholder={text.messagePlaceholderText}
+        required
+        maxLength="1000"
+      />
+      <div data-netlify-recaptcha="true"></div>
+      <button type="submit" className="Contact__form__button">
+        {text.sendButtonText}
+      </button>
+    </form>
+  );
+};
 
 export default ContactForm;
