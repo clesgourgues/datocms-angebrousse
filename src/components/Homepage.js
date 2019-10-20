63a8bc013d7703fbfc3dea257e2cad5b28b75fea
cecile lesgourgues <cecile.lesgourgues@ekino.com>
2019-10-20 23:33:24 +0200

    stop slider on click

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,12 +4,22 @@ import Header from "@components/Header";
 
 const Layout = ({ children, images, menu }) => {
   const [selected, setSelected] = useState(0);
+  const [stop, setStop] = useState(false);
+  useEffect(() => {
+    document.body.addEventListener("click", handleClick);
+  }, []);
+
+  const handleClick = () => {
+    setStop(true);
+  };
   useEffect(() => {
     const next = selected === images.slider.length - 1 ? 0 : selected + 1;
-    const interval = setInterval(() => {
-      setSelected(next);
-    }, 3000);
-    return () => clearInterval(interval);
+    if (!stop) {
+      const interval = setInterval(() => {
+        setSelected(next);
+      }, 3000);
+      return () => clearInterval(interval);
+    }
   });
   return (
     <BackgroundImage Tag="section" fluid={images.slider[selected].fluid} className="Container">
