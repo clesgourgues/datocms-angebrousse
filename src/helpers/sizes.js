64d152f56f998e6b04cafc9a680abb48814ba3f6
cecile lesgourgues <cecile.lesgourgues@ekino.com>
2020-09-07 20:17:31 +0200

    fix: fix snipcart sizes options

--- src/helpers/sizes.js
+++ src/helpers/sizes.js
@@ -1,10 +1,11 @@
-export const sizes = ["48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60"];
+export const sizes = ['48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60'];
 
 export const getProductOptions = (sizes, size) => {
   const filteredSizes = sizes.filter(s => s !== size);
   const orderedSizes = [size, ...filteredSizes];
   return orderedSizes
+    .filter(s => s !== null)
     .toString()
-    .split(",")
-    .join("|");
+    .split(',')
+    .join('|');
 };
